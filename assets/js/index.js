f4a8e058bae2f9eeb3ac577613a7804ac19ef9c1
Stefan019 <stefanstevanovic019@gmail.com>
2024-04-23 22:23:58 +0200

    added js & css functionality, deleted junk

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// burger
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
 
@@ -11,20 +12,31 @@ document.querySelectorAll(".navlink").forEach(n =>
         navMenu.classList.remove("active");
     }))
 
-/*
+//  Header
+var lastScrollTop = 0;
 
-Transparentan header i footer, 
-drugacije boje generalno,
-slika neka kao pozadina,
-video u sredini sa teksom pored (predpostavljam 2 razlicita diva sa flexom)
-checkout stranica je jos potrebna
-sekcija service (?) about, text i fotka 
-3 diva sa buttonom ka checkoutu, servisi u njima
-footer??? ne znam bajo
-napravi git
-uci js
-uci react
+    window.addEventListener("scroll", function() {
+        var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-
-
-*/
+        if (currentScroll > lastScrollTop) {
+            document.querySelector(".header").classList.remove("visible");
+        } 
+        else {
+            document.querySelector(".header").classList.add("visible");
+        }
+    
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+    }, false);
+    
+window.addEventListener("scroll", function() {
+    var headerVisible = document.querySelector(".header").classList.contains("visible");
+    var navScroll = document.querySelector(".navMenu");
+    
+    if (!headerVisible) {
+        navScroll.style.top = "0px";
+    } 
+    else {
+        navScroll.style.top = "70px";
+        }
+    }, false);
+    
