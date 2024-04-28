ab8ccdb230cc365a1e72fd3b4fbc63c82c325a55
Stefan019 <stefanstevanovic019@gmail.com>
2024-04-28 17:51:31 +0200

    added functionality for checkout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,16 @@
 // burger  //
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
-
-hamburger.addEventListener("click", ()=>{
+try{
+    hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+}
+catch(error){
+    console.error(error);
+}
+
 document.querySelectorAll(".navlink").forEach(n => 
     n.addEventListener("click", ()=>{
         hamburger.classList.remove("active");
@@ -58,8 +63,63 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 // Checkout //
+function checkoutFunction(){
+    
+    let buttons = document.querySelectorAll(".buttons");
 
-const checkoutPack = document.querySelectorAll("element");
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
 
-const completePurchase = document.getElementById('completePurchase');
+            let contentId = button.id.replace("button", "elTitles");
+            let textId = button.id.replace("button", "elPrices");
+
+            let content = document.getElementById(contentId).innerText;
+            let textSwap = document.getElementById(textId).innerText;
+
+
+            switch(button.id){
+                case "button1":
+                    window.location.href = "checkout.html?content=" + encodeURIComponent(content) + 
+                    "&textSwap=" + encodeURIComponent(textSwap);
+                    console.log("Button 1");
+
+                break;
+            case "button2":
+                window.location.href = "checkout.html?content=" + encodeURIComponent(content) + 
+                "&textSwap=" + encodeURIComponent(textSwap);
+                    console.log("Button 2");
+                break;
+            case "button3":
+                window.location.href = "checkout.html?content=" + encodeURIComponent(content) + 
+                "&textSwap=" + encodeURIComponent(textSwap);
+                    console.log("Button 3");
+                break;
+                
+            default: 
+                console.log("Watafak?!");
+            }
+        })
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    let queryParams = new URLSearchParams(window.location.search);
+    let content = queryParams.get("content");
+    let textSwap = queryParams.get("textSwap");
+
+
+    if (content && textSwap) {
+        document.getElementById("span-text").innerText = content;
+        
+        let updatedTextSwap = document.querySelectorAll(".checkoutPrices");
+        updatedTextSwap.forEach(function(element, index) {
+            element.innerText = textSwap;
+        });
+    }
+});
+
+
+
+// const completePurchase = document.getElementById('completePurchase');
 
